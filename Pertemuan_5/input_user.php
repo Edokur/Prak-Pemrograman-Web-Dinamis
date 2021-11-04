bc209644376ce0228fb8eb2e3be88a4b1728d4ed
Edo Kurniawan <52836959+Edokur@users.noreply.github.com>
2021-11-04 21:52:52 +0700

    Peretmuan 5

---
+++ Pertemuan_5/input_user.php
@@ -0,0 +1,11 @@
+<?php
+include "../Pertemuan_3/koneksi.php";
+$id_user = $_POST['id_user'];
+$nama = $_POST['nama_lengkap'];
+$email = $_POST['email'];
+$pass = md5($_POST['password']);
+$sql = "INSERT INTO users(id_user, nama_lengkap, email, password) VALUES ('$id_user', '$nama','$email', '$pass')";
+$query=mysqli_query($con, $sql);
+mysqli_close($con);
+header('location:tampil_user.php');
+?>
